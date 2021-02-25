230f04e30ccb523d166c2d12f1ef8c1c10181862
HugodiazC <78826201+HugodiazC@users.noreply.github.com>
2021-02-25 11:56:03 -0600

    Update Paint with cv2.py

--- OPENCV_Tutorial/Paint with cv2.py
+++ OPENCV_Tutorial/Paint with cv2.py
@@ -1,15 +1,15 @@
 import cv2
 import numpy as np
 
-#img= np.zeros((512,512)) #los zeros son negros los unos son blanco
+#img= np.zeros((512,512)) #zeros son negros los unos son blanco
 #Resultado de esto imagen completamente negra.
 
-#img2= np.zeros((512,512,3),np.uint8) # esto es para imagen a color
+#img2= np.zeros((512,512,3),np.uint8) #imagen a color
 #img3= np.zeros((512,512,3),np.uint8)
 img4= np.zeros((512,512,3),np.uint8)
 #print(img2)
-#img2[:]=255,0,0 #esto la pinta de azul
-#img3[200:300,100:300]=255,0,0 #esto la pinta de azul
+#img2[:]=255,0,0 #Pintar de azul
+#img3[200:300,100:300]=255,0,0 #Pintar de azul
 
 cv2.line(img4,(0,0),(300,300),(0,255,0),3)
 
